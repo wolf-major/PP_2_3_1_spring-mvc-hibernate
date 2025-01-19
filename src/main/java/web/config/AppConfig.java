e54a450e27b3a8d54713180606d6a3fd2192bb29
Daria Prudnikova <daria2997@gmail.com>
2025-01-19 14:44:20 +0300

    2.3.1

---
+++ src/main/java/web/config/AppConfig.java
@@ -0,0 +1,35 @@
+package web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:db.properties")
+@ComponentScan(value = "web")
+public class AppConfig {
+	private final Environment environment;
+
+	@Autowired
+	public AppConfig(Environment environment) {
+		this.environment = environment;
+	}
+
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getProperty("db.driver"));
+		dataSource.setUrl(environment.getProperty("db.url"));
+		dataSource.setUsername(environment.getProperty("db.username"));
+		dataSource.setPassword(environment.getProperty("db.password"));
+		return dataSource;
+	}
+
+}
