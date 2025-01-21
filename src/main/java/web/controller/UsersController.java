084fa5fb565a9297d51aca757d5cd3d3f1d25b0e
wolf-major <daria2997@gmail.com>
2025-01-21 16:41:31 +0300

    intermediate result

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.User;
 import web.service.UserService;
-import web.service.UserServiceImp;
+
+import java.util.Map;
 
 @Controller
 @Validated
@@ -22,6 +27,26 @@ public class UsersController {
     public String getUsers(Model model) {
         model.addAttribute("title", "Список пользователей:");
         model.addAttribute("userList", userService.getUsers());
-        return "userList";
+        return "user_list";
     }
+
+    @GetMapping(value = "/addUser")
+    public String newUserForm(Map<String, Object> model) {
+        User user = new User();
+        model.put("newUser", user);
+        return "new_user";
+    }
+
+    @PostMapping(value = "/save")
+    public String saveUser(@ModelAttribute("newUser") User user) {
+        userService.addUser(user);
+        return "redirect:/users";
+    }
+
+
+    @GetMapping(value = "/edit")
+    public String editUser(@RequestParam(value = "id") Integer id, Model model) {
+        return "userEditpage";
+    }
+
 }
--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 public class User {
 	@Id
 	@Column(name="id")
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	@Column(name="firstName")
